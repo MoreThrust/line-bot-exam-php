0169fd2a666209c547b0aae4542e058f9f1afe7b
MoreThrust <peeraphat1997@gmail.com>
2018-04-19 14:37:31 +0700

    Koo

--- webhooks.php
+++ webhooks.php
@@ -18,19 +18,14 @@ if (!is_null($events['events'])) {
 			// Get text sent
 			if($event['message']['text'] == 'แสงสว่าง'){
 				$replyToken = $event['replyToken'];
-
-				$messages = [
-					'type' => 'text',
-					'text' => "koo"
-				];
-/*				
+				
 				$messages = [
 					"type" => "image",
 					"originalContentUrl" => "https://scontent.fbkk1-2.fna.fbcdn.net/v/t1.0-1/c0.1.100.100/p100x100/29343246_450150078777035_7169313599809126400_n.jpg?_nc_cat=0&oh=f9baa54cfe0e04cb0ba0c6bf580e6e80&oe=5B6616B3",
 					"previewImageUrl" => "https://scontent.fbkk1-2.fna.fbcdn.net/v/t1.0-1/c0.1.100.100/p100x100/29343246_450150078777035_7169313599809126400_n.jpg?_nc_cat=0&oh=f9baa54cfe0e04cb0ba0c6bf580e6e80&oe=5B6616B3"
 				];
 			}
-
+/*
 			if($event['message']['text'] == 'แอร์'){
 				$replyToken = $event['replyToken'];
 				
