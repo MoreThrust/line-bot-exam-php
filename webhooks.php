171d5ad5bc796eacc487127e5fb6cd5c81f51da4
MoreThrust <peeraphat1997@gmail.com>
2018-04-21 23:24:01 +0700

    koo

--- webhooks.php
+++ webhooks.php
@@ -24,9 +24,15 @@ if (!is_null($events['events'])) {
 					"template" => [
 						"type" => "confirm",
 						"actions" => [
-							"type" => "message",
-							"label" => "Yes",
-							"text" => "Yes"
+							[
+								"type" => "message",
+								"label" => "Yes",
+								"text" => "Yes"
+							],[
+								"type" => "message",
+								"label" => "No",
+								"text" => "No"
+							]
 						],
 						"text": "Continue?"
 					]
